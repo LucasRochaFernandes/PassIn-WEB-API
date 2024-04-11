4b40bd32d0c28900d30e2351075d24f7ad3c1b35
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2024-04-10 22:53:14 -0300

    add filters in Api project

---
+++ PassIn.Api/Filters/ExceptionFilter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+using System.Net;
+
+namespace PassIn.Api.Filters;
+
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        var result = context.Exception is PassInException;
+        if (result)
+        {
+            HandleException();
+        }
+        else
+        {
+            ThrowUnknownError(context);
+        }
+    }
+    private void HandleException()
+    {
+
+    }
+    private void ThrowUnknownError(ExceptionContext context)
+    {
+        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        context.Result = new ObjectResult(new ResponseErrorJson("Unknown Error"));
+    }
+}
+
